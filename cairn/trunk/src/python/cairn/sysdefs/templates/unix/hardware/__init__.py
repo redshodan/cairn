08c0844d5f60535143568c2588f90931fb872e30
baron <baron@fe15d88a-a327-0410-a6be-1ddfdd94247e>
2006-02-25 15:49:12 +0000

    Move moving. Wish you could do all the moving bits with one commit

--- cairn/trunk/src/python/cairn/sysdefs/templates/unix/hardware/__init__.py
+++
@@ -1,9 +0,0 @@
-"""templates.unix.hardware Module"""
-
-
-import cairn
-
-
-
-def getSubModuleString(sysdef):
-	return "Drives; Partitions; FSTab; DiskUsage; Summary"
---
+++ cairn/trunk/src/python/cairn/sysdefs/templates/unix/system/partitions/__init__.py
@@ -0,0 +1,9 @@
+"""templates.unix.system.partitions Module"""
+
+
+import cairn
+
+
+
+def getSubModuleString(sysdef):
+	return "List; FSTab; DiskUsage; Summary"
