4d7300ca071d102ec512e86408bbe05c689885d6
baron <baron@fe15d88a-a327-0410-a6be-1ddfdd94247e>
2006-06-30 02:49:25 +0000

    Map the devices in sfdisk config file. make sure sfdisk -l warnings dont make it into the config file.

--- cairn/trunk/src/python/cairn/sysdefs/linux/unknown/restore/setup/PartDrives.py
+++ cairn/trunk/src/python/cairn/sysdefs/linux/unknown/restore/setup/PartDrives.py
@@ -21,6 +21,7 @@ class PartDrives(tmpl.PartDrives):
 		device = drive.get("mapped-device")
 		cmd = "%s --force %s < %s" % (sysdef.info.get("env/tools/part"), device,
 									  cfgFile)
+		cairn.verbose("running: %s" % cmd)
 		ret = commands.getstatusoutput(cmd)
 		if ret[0] != 0:
 			msg = "Failed to run %s to partition drive %s: %s" % \
@@ -33,7 +34,10 @@ class PartDrives(tmpl.PartDrives):
 	def writePartCfg(self, sysdef, drive):
 		cfgFile = tempfile.mkstemp("", "cairn-")
 		cairn.addFileForCleanup(cfgFile[1])
+		dev = drive.get("device")
+		mappedDev = drive.get("mapped-device")
 		contents = "%s\n" % drive.get("part-tool-cfg")
+		contents = contents.replace(dev, mappedDev)
 		os.write(cfgFile[0], contents)
 		os.close(cfgFile[0])
 		return cfgFile[1]
--- cairn/trunk/src/python/cairn/sysdefs/linux/unknown/system/partitions/List.py
+++ cairn/trunk/src/python/cairn/sysdefs/linux/unknown/system/partitions/List.py
@@ -3,6 +3,8 @@
 
 import commands
 import re
+import tempfile
+import os
 
 import cairn
 import cairn.sysdefs.templates.unix.system.partitions.List as tmpl
@@ -28,15 +30,21 @@ class List(tmpl.List):
 
 
 	def definePartitions(self, sysdef, drive):
-		cmd = "%s -uS -d %s" % (sysdef.info.get("env/tools/part"),
-								drive.get("device"))
+		cfgFile = tempfile.mkstemp("", "cairn-")
+		cairn.addFileForCleanup(cfgFile[1])
+		cmd = "%s -uS -d %s > %s" % (sysdef.info.get("env/tools/part"),
+									 drive.get("device"), cfgFile[1])
 		ret = commands.getstatusoutput(cmd)
 		if ret[0] != 0:
 			msg = "Failed to run %s to find drive information:\n" % sysdef.info.get("env/tools/part")
 			raise cairn.Exception(msg + ret[1])
-		drive.setChild("part-tool-cfg", ret[1])
+		if len(ret[1]) > 0:
+			cairn.verbose("sfdisk output: %s" % ret[1])
+		content = os.read(cfgFile[0], 1000000)
+		os.close(cfgFile[0])
+		drive.setChild("part-tool-cfg", content)
 		partNum = 1
-		for line in ret[1].split("\n"):
+		for line in content.split("\n"):
 			if line.startswith("/dev/"):
 				if line.endswith("Id= 0"):
 					partNum = partNum + 1
