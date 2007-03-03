0f37fa2178981ad718510a055c8295edac6132dd
redshodan <redshodan@fe15d88a-a327-0410-a6be-1ddfdd94247e>
2007-03-03 16:19:29 +0000

    r244@minimoose:  baron | 2007-03-03 09:18:15 -0700  Skip nvidia raid detect for now since it causes segfault sometimes

--- src/linux/volumeid/volume_id/nvidia_raid.c
+++ src/linux/volumeid/volume_id/nvidia_raid.c
@@ -49,8 +49,8 @@ int volume_id_probe_nvidia_raid(struct volume_id *id, uint64_t off, uint64_t siz
 
 	meta_off = ((size / 0x200)-2) * 0x200;
 	buf = volume_id_get_buffer(id, off + meta_off, 0x200);
-	if (buf == NULL)
-		return -1;
+	//if (buf == NULL)
+    return -1;
 
 	nv = (struct nvidia_meta *) buf;
 	if (memcmp(nv->vendor, NVIDIA_SIGNATURE, sizeof(NVIDIA_SIGNATURE)-1) != 0)
